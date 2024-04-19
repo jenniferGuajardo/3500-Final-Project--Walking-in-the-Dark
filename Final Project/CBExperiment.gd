c1303740835b8ceb238a9929a3ea7375ea7b27d9
jenniferGuajardo <156961987+jenniferGuajardo@users.noreply.github.com>
2024-04-19 12:50:03 -0400

    Third Iteration

--- Final Project/CBExperiment.gd
+++ Final Project/CBExperiment.gd
@@ -1,3 +1,8 @@
+### EXPERIMENT ###
+
+# I wanna see what happens when I adjust the physics.
+
+
 extends CharacterBody3D
 
 
@@ -141,8 +146,8 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("leftward", "rightward", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * SPEED * delta
+		velocity.z = direction.z * SPEED * delta
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
